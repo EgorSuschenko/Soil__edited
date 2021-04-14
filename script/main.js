04dbf4aee8302d347c76c7676d2fcc1d6c8b9a87
Sergey <my_name_sprite@mail.ru>
2021-04-14 17:40:31 +0300

    fixed modal windows issues

--- script/main.js
+++ script/main.js
@@ -5,6 +5,7 @@ for(let anchor of anchors) {
   anchor.addEventListener("click", function(event) {
     event.preventDefault() 
     const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    console.log(goto);
     document.querySelector(goto).scrollIntoView({
       behavior: "smooth",
       block: "start"
--- script/sign_up_modal.js
+++ script/sign_up_modal.js
@@ -1,16 +1,41 @@
+const modalReg = document.getElementById("regModal");
+const btnReg = document.getElementById("registration");
+const spanReg = document.getElementsByClassName("close_btn")[0];
+
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("myBtn");
 const span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
+  document.querySelector("body").style.overflow = "hidden" 
 }
 
 span.onclick = function() {
   modal.style.display = "none";
+  document.querySelector("body").style.overflow = "inherit"
 }
 
 window.onclick = (event) => {
-  if (event.target == modal) {
+  if (event.target == modal || event.target == modalReg) {
     modal.style.display = "none";
+    modalReg.style.display = "none";
+    document.querySelector("body").style.overflow = "inherit"
   }
 }
+
+
+btnReg.onclick = function() {
+  modalReg.style.display = "block";
+  modal.style.display = "none";
+}
+
+spanReg.onclick = function() {
+  modalReg.style.display = "none";
+  document.querySelector("body").style.overflow = "inherit"
+}
+
+//  window.onclick = (event) => {
+//   if (event.target == modalReg) {
+//     modalReg.style.display = "none";
+//   }
+//  }
