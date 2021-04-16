8a43dec9d595017460ddbc0172db18564d1255aa
Egor Suschenko <lalalayes2@gmail.com>
2021-04-16 17:28:45 +0300

    feat: carousel animation and right transition

--- script/carousel.js
+++ script/carousel.js
@@ -1,8 +1,8 @@
-let width = 250; // ширина картинки
+let width = 262; // ширина картинки
     let count = 3; // видимое количество изображений
 
     let list = document.querySelector('.example_wrapper');
-    let listElems = document.querySelectorAll('.partner_block');
+    let listElements = document.querySelectorAll('.partner_block');
 
     let position = 0; // положение ленты прокрутки
 
@@ -11,13 +11,13 @@ let width = 250; // ширина картинки
       position += width * count;
       // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
       position = Math.min(position, 0)
-      list.style.marginLeft = position + 'px';
+      list.style.transform = `translateX(${position + 'px'})`;
     };
 
     document.querySelector('.next').onclick = function() {
       // сдвиг вправо
       position -= width * count;
       // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
-      position = Math.max(position, -width * (listElems.length - count));
-      list.style.marginLeft = position + 'px';
+      position = Math.max(position, -width * (listElements.length - count));
+      list.style.transform = `translateX(${position + 'px'})`;
     };
