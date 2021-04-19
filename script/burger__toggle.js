d034ed88db3777c90723baaf5ae6bb153449301c
Egor Suschenko <lalalayes2@gmail.com>
2021-04-19 22:14:22 +0300

    fix: butons disappear

--- script/burger__toggle.js
+++ script/burger__toggle.js
@@ -10,5 +10,9 @@ let toggleBurger = () => {
   document.querySelector('.navigation__list').classList.toggle('navigation__list-small');
   Array.from(document.querySelectorAll('.nav__text'))
   .forEach(item => item.classList.toggle('text__small'));
+
+  document.querySelector('.field__btn__add').classList.toggle('display__none');
+  document.querySelector('.field__btn__done').classList.toggle('display__none');
+  document.querySelector('.field__btn__delete').classList.toggle('display__none');
 }
 burger.addEventListener('click', toggleBurger);
